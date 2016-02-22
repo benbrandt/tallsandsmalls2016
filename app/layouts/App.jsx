3b3611b6d93e72575eb31d038983838f7b1ee1ce
Ben Brandt <benjamin.j.brandt@gmail.com>
2016-02-21 21:29:13 -0800

    Update to styles

--- app/layouts/App.jsx
+++ app/layouts/App.jsx
@@ -1,12 +1,22 @@
 import content from '../data/content';
 import Menu from '../components/Menu.jsx';
 import React, { Component } from 'react';
-import { StyleRoot } from 'radium';
+import { Style, StyleRoot } from 'radium';
+import variables from '../styles/variables.js';
 
 class App extends Component {
   render() {
     return (
       <StyleRoot>
+        <Style
+          rules={{
+            body: {
+              background: variables.colors.champagne,
+              color: variables.colors.gray,
+              fontFamily: variables.font.families.cursive,
+            },
+          }}
+        />
         <Menu data={content.menu} />
       </StyleRoot>
     );
---
+++ app/styles/variables.js
@@ -0,0 +1,36 @@
+const variables = {
+  colors: {
+    black: '#1D2326',
+    burgandy: '#611427',
+    champagne: '#DDDCC5',
+    gray: '#6A6A61',
+    tan: '#958976',
+
+    primary: variables.colors.burgandy,
+  },
+
+  font: {
+    families: {
+      cursive: 'Pacifico, cursive',
+      sansSerif: 'Roboto, sans-serif',
+    },
+    sizes: {
+      1: '3rem',
+      2: '2.5rem',
+      3: '1.75rem',
+      4: '1.5rem',
+      5: '1.25rem',
+      6: '1rem',
+    },
+  },
+
+  misc: {
+    columnGap: '1.25rem',
+    easing: 'ease-out',
+    headerHeight: '3.125rem',
+    radius: '.25rem',
+    speed: '86ms',
+  },
+};
+
+export default variables;
