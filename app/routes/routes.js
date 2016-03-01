360324be4218445fcf316ea7eb40766c2d521d4e
Ben Brandt <benjamin.j.brandt@gmail.com>
2016-02-29 23:56:51 -0800

    Add routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router';
-import App from './layouts/App.jsx';
-import Details from './layouts/Details.jsx';
-import Rsvp from './layouts/Rsvp.jsx';
+import App from '../layouts/App.jsx';
+import Details from '../layouts/Details.jsx';
+import Rsvp from '../layouts/Rsvp.jsx';
 
 module.exports = (
   <Route path="/" component={App}>
