442c3443eaceaea19ef40ca454ada996bd1687ad
Ben Brandt <benjamin.j.brandt@gmail.com>
2016-02-29 23:24:38 -0800

    Add routes

--- app/main.jsx
+++ app/main.jsx
@@ -1,9 +1,10 @@
-import App from './layouts/App.jsx';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes/routes';
 
-function main() {
-  ReactDOM.render(<App />, document.getElementById('app'));
-}
+render(
+  <Router routes={routes} history={browserHistory} />,
+  document.getElementById('app')
+);
 
-main();
---
+++ app/routes/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router';
+import App from './layouts/App.jsx';
+import Details from './layouts/Details.jsx';
+import Rsvp from './layouts/Rsvp.jsx';
+
+module.exports = (
+  <Route path="/" component={App}>
+    <Route path="/details" component={Details} />
+    <Route path="/rsvp" component={Rsvp} />
+  </Route>
+);
