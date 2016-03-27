4bb9efafd51d6c5b89cec2cb92f73f1a81301e18
Ben Brandt <benjamin.j.brandt@gmail.com>
2016-03-26 17:00:53 -0700

    Move sections into loop

--- app/components/Rsvp/Rsvp.js
+++ app/components/Rsvp/Rsvp.js
@@ -1,26 +1,71 @@
 import React, { Component, PropTypes } from 'react';
+import CSSModules from 'react-css-modules';
+import styles from './Rsvp.css';
 
-export default class Rsvp extends Component {
-  componentWillMount() {
-    this.setState.submitted = false;
+class Rsvp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      attending: false,
+      gotcha: '',
+      guest: '',
+      seats: 0,
+      submitted: false,
+    };
+    this.handleAttending = this.handleAttending.bind(this);
+    this.handleGotcha = this.handleGotcha.bind(this);
+    this.handleGuest = this.handleGuest.bind(this);
+    this.handleSeats = this.handleSeats.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  handleAttending(e) {
+    this.setState({ attending: e.target.checked });
+  }
+
+  handleGotcha(e) {
+    this.setState({ gotcha: e.target.value });
+  }
+
+  handleGuest(e) {
+    this.setState({ guest: e.target.value });
+  }
+
+  handleSeats(e) {
+    this.setState({ seats: e.target.value });
   }
 
   submitForm(e) {
     e.preventDefault();
-    this.setState.submitted = true;
-    alert('Thanks for letting us know!');
+
+    const data = {
+      _subject: 'New RSVP from TallsandSmalls2016.com',
+      _cc: this.props.cc,
+      guest: this.state.guest,
+      seats: this.state.seats,
+      attending: this.state.attending ? 'Attending: Yes' : 'Attending: No',
+      _gotcha: this.state.gotcha,
+    };
+
+    const request = new XMLHttpRequest();
+    request.open('POST', `https://formspree.io/${this.props.email}`, true);
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    request.send(JSON.stringify(data));
+
+    this.setState({ submitted: true });
   }
 
   render() {
     return (
-      <div className="inner">
-        <h3>
-          <img src="assets/date.png" />
-          RSVP
-        </h3>
+      <div>
+        <h3>RSVP</h3>
+
+        <p hidden={!this.state.submitted}>
+          Thanks for letting us know!
+        </p>
 
         <form
-          hidden="submitted"
+          hidden={this.state.submitted}
           id="rsvp"
           name="rsvp"
         >
@@ -29,24 +74,35 @@ export default class Rsvp extends Component {
             name="guest"
             required
             placeholder="Name of guest"
+            onChange={this.handleGuest}
           />
 
           <input
-            type="text"
+            hidden={!this.state.attending}
+            type="number"
             placeholder="Number of Seats"
             name="seats"
+            onChange={this.handleSeats}
           />
 
           <input
             type="checkbox"
             id="attending"
             name="attending"
+            defaultValue={this.state.attending}
+            onChange={this.handleAttending}
           />
-          <label forInput="attending">Attending</label>
+          <label htmlFor="attending">Attending</label>
+
+          <input
+            type="text"
+            name="_gotcha"
+            hidden
+            onChange={this.handleGotcha}
+          />
 
           <button
             type="submit"
-            disabled={this.state.valid}
             onClick={this.submitForm}
           >
               Submit
@@ -58,7 +114,11 @@ export default class Rsvp extends Component {
 }
 
 Rsvp.propTypes = {
+  cc: PropTypes.string,
+  email: PropTypes.string.isRequired,
   name: PropTypes.string,
   seats: PropTypes.number,
-  attending: PropTypes.boolean,
+  attending: PropTypes.bool,
 };
+
+export default CSSModules(Rsvp, styles);
--- app/layouts/App.js
+++ app/layouts/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import content from '../data/content.js';
 import Header from '../components/Header/Header';
 import Parallax from '../components/Parallax/Parallax';
+import Rsvp from '../components/Rsvp/Rsvp';
 import Section from '../components/Section/Section';
 
 const App = () => {
@@ -9,70 +10,32 @@ const App = () => {
   const groom = content.details.party.groom.substr(0, content.details.party.groom.indexOf(' '));
   const names = `${groom} & ${bride}`;
 
+  const sections = content.sections.map(section => (
+    <div key={section.id}>
+      <Parallax index={section.id} bgImage={section.parallax} />
+      <Section src={section.img}>
+        <h3 dangerouslySetInnerHTML={{ __html: section.header }} />
+        <p dangerouslySetInnerHTML={{ __html: section.copy }} />
+      </Section>
+    </div>
+  ));
+
   return (
     <div>
       <Header
         names={names}
       />
-      <Parallax index="0" bgImage="assets/home.jpg" />
-      <Section src="assets/hearts.png">
-        <h3>We met through friends, well kinda...</h3>
-        <p>
-          Kansas City is a small-big town, we had so many mutual friends but
-          had never met. Heather's profile picture captured Austin's eye
-          one day as he was on Facebook, after a few messages back and forth
-          we met for a lunch date and were inseparable even since.
-        </p>
+
+      {sections}
+
+      <Parallax index="rsvp" bgImage="assets/home.jpg" />
+
+      <Section src="assets/date.png">
+        <Rsvp
+          email={content.contact.email}
+          cc={content.contact.cc}
+        />
       </Section>
-
-      <Parallax index="1" bgImage="assets/home.jpg" />
-
-      <Section src="assets/Church.png">
-        <h3>
-          Celebrate with us on June 17, 2016<br />
-          <small>5:00 - 10:00 PM</small>
-        </h3>
-
-        <p>
-          Palm Beach Gardens<br />
-          343 N Federal Hwy<br />
-          Palm Beach, FL<br />
-          <a href="https://goo.gl/maps/df" target="_blank">Map</a>
-        </p>
-      </Section>
-
-      <Parallax index="2" bgImage="assets/home.jpg" />
-
-      <Section src="assets/travel.png">
-        <h3>Travel and Accommodations</h3>
-
-        <p>With 2 different airports close by, you can choose
-        between: <a href="http://www.fll.net" target="_blank">Fort Lauderdale–Hollywood International Airport</a> or
-        <a href="http://www.pbia.org" target="_blank">Palm Beach International Airport</a></p>
-
-        <p>
-          Delray Beach is a gorgeous destination with so many
-          options for accomodations, we decided to leave
-          it up to the guests to pick their accomodations. A few
-          recommended choices are:
-          <a href="http://www.marriott.com/hotels/travel/pbidr-delray-beach-marriott" target="_blank">Delray Beach Marriott</a>,
-          <a href="http://www.marriott.com/hotels/travel/pbiri-residence-inn-delray-beach" target="_blank">Residence Inn Delray Beach</a>
-          or <a href="http://www.theseagatehotel.com" target="_blank">The Seagate Hotel and Spa</a>.
-        </p>
-      </Section>
-
-      <Parallax index="3" bgImage="assets/home.jpg" />
-
-      <Section src="assets/gift.png">
-        <h3>Registry</h3>
-
-        <p>
-          We are registered at:
-          <a href="http://williams-sonoma.com" target="_blank">Williams Sonoma</a>, <a href="http://potterybarn.com" target="_blank">Pottery Barn</a> and <a href="http://target.com" target="_blank">Target</a>
-        </p>
-      </Section>
-
-      <Parallax index="4" bgImage="assets/home.jpg" />
     </div>
   );
 };
