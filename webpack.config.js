5d00ccce8edab9070fbcc4cbe51bc1d43e9c7ae4
Ben Brandt <benjamin.j.brandt@gmail.com>
2016-04-04 20:20:46 -0700

    Add production environment to npm build

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,11 @@ const config = {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
     new ExtractTextPlugin('app.css', {
       allChunks: true,
     }),
